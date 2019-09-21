9d79d9f414e9d163417c34e167476022b3617ddd
Jeffery White <40738462+Bevanwhite@users.noreply.github.com>
2019-09-21 16:47:06 +0530

    message board created

--- src/components/message_board/MessageBoard.js
+++ src/components/message_board/MessageBoard.js
@@ -1,11 +1,44 @@
 import React from 'react';
-import SpacingGrid from './Grid.js';
+import Container from './container'
+import './style.css'
 
 function MessageBoard() {
   return (
     <div>
-        <h1>Message Board</h1>
-        <SpacingGrid />
+        <h1 className='App'>Message Board</h1>
+        <Container
+          questionData={{
+          id: "01",
+          imgUrl: "http://placekitten.com/200/200?image=11",
+          question: "How can i cook rice?",
+          reply: "You have wash the with water and cook it"
+          }}
+        />
+        <Container
+          questionData={{
+          id: "02",
+          imgUrl: "http://placekitten.com/200/200?image=8",
+          question: "How can i cook rice?",
+          reply: "You have wash the with water and cook it"
+          }}
+        />
+        <Container
+          questionData={{
+          id: "03",
+          imgUrl: "http://placekitten.com/200/200?image=7",
+          question: "How can i cook good marten curry?",
+          reply: "You have wash the with water and cook it"
+          }}
+        />
+        <Container
+          questionData={{
+          id: "04",
+          imgUrl: "http://placekitten.com/200/200?image=4",
+          question: "How can you love a dog?",
+          reply: "You have wash the with water and cook it"
+          }}
+        />
+        <hr/>
     </div>
   );
 }
---
+++ src/components/message_board/questionData.js
@@ -0,0 +1,30 @@
+const questionData = [
+    {
+        id: "01",
+        imgUrl: "http://placekitten.com/200/200?image=4",
+        question: "how can i cook rice?",
+        reply: "you have wash the rice with water and cook it"
+        
+    },
+    {
+        id: "02",
+        imgUrl: "http://placekitten.com/200/200?image=8",
+        question: "How can i cook rice?",
+        reply: "You have wash the with water and cook it"
+    },
+    {
+        id: "03",
+        imgUrl: "http://placekitten.com/200/200?image=7",
+        question: "How can i cook good marten curry?",
+        reply: "You have wash the with water and cook it"
+    },
+    {
+        id: "04",
+        imgUrl: "http://placekitten.com/200/200?image=4",
+        question: "How can you love a dog?",
+        reply: "You have wash the with water and cook it"
+    }
+
+]
+
+export default questionData
