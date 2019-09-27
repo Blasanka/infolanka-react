3d8cc6d901159e37b6f28af10983e84a7d5862d9
AkithaIdd <47883855+AkithaIdd@users.noreply.github.com>
2019-09-27 16:09:45 +0530

    joke page

---
+++ src/joke/jokeall/Joke15.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class HelloWorld extends React.Component {
+  render() {
+    return <p>Mr.Kanjibhai was travelling to Bombay to take up a new job. Ms.Rupaben was also travelling in the same compartment.They didn't know each other in the beginning but Once they started talking, they realized they both had a lot in common
+    a.. Both were Single.
+    b.. Both were Gujaratis.
+    c.. Both were going to Bombay .
+    d.. Both were teachers.
+    e.. Both were starting new jobs at Mithibai College ...
+    They seemed to fit very well and decided to be roommates in Bombay and made a pact that they would do everything together..
+    So they lived in the same house, travelled to the college together on Kanjibhai's scooter, had lunch in the staff room together, returned home together.They were watching the TV together, eating dinner together and were also sharing the same bedroom and...EVEN sharing the same bed.
+    The only problem was that Rupaben was placing a pillow between them at bedtime, much to the frustration of Kanjibhai, who ended up spending many sleepless nights with this most desirable beauty besides him, separated by the pillow.
+    Kanjibhai's frustration built up to such an extent that he could take it no more and ended up deciding to drink.So one day he took off from the college leaving Rupaben on her own. She was quite upset, but made it home by auto rickshaw.
+    The pact had been broken so she decided not to open the door for Kanjibhai when he got home drunk at about 2 AM. Kanjibhai knocked on the door for about 20 minutes and pleaded with his roommate to let him in.
+    After listening to his crap for 20 minutes, Rupaben said, ' Where the hell did you disappear today? We decided to do everything together! .From now onwards you will sleep outside'.
+    Kanjibhai said, 'I will jump over the wall and come in if you don't open the door right now!'
+    To this Rupaben replied.
+    "You could not jump over a pillow for four months and now you are talking of jumping over the wall!! "</p>;
+  }
+}
+
+export default HelloWorld;
