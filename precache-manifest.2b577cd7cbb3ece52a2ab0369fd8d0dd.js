b64d3dc2f060f5d213842af5cc76f934cf0009ac
blasanka <blasanka95@gmail.com>
2019-09-30 21:26:38 +0530

    Updates

---
+++ precache-manifest.2b577cd7cbb3ece52a2ab0369fd8d0dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d74d41bd890b3ad7facb57b9e1b98888",
+    "url": "/modern-infolanka/index.html"
+  },
+  {
+    "revision": "4ea143e52caab8277d4e",
+    "url": "/modern-infolanka/static/css/2.0e2ecc17.chunk.css"
+  },
+  {
+    "revision": "1ecdf7104cfc707e6fdc",
+    "url": "/modern-infolanka/static/css/main.eda912ec.chunk.css"
+  },
+  {
+    "revision": "4ea143e52caab8277d4e",
+    "url": "/modern-infolanka/static/js/2.e894f888.chunk.js"
+  },
+  {
+    "revision": "1ecdf7104cfc707e6fdc",
+    "url": "/modern-infolanka/static/js/main.221db139.chunk.js"
+  },
+  {
+    "revision": "ca6b45a310a15804c03c",
+    "url": "/modern-infolanka/static/js/runtime~main.d53e09aa.js"
+  }
+]);
