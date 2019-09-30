b77e335372a57c21733cf1397f5f64881fcd015a
blasanka <blasanka95@gmail.com>
2019-09-30 21:36:33 +0530

    Updates

---
+++ precache-manifest.273ada15aaa6b4ca7fc260c8fafe3777.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d939df951acd8f3c8623f08017edae39",
+    "url": "/infolanka-react/index.html"
+  },
+  {
+    "revision": "142aea5704b0ec933ac7",
+    "url": "/infolanka-react/static/css/2.0e2ecc17.chunk.css"
+  },
+  {
+    "revision": "8fa07a5d1fb850938b86",
+    "url": "/infolanka-react/static/css/main.eda912ec.chunk.css"
+  },
+  {
+    "revision": "142aea5704b0ec933ac7",
+    "url": "/infolanka-react/static/js/2.fad9607b.chunk.js"
+  },
+  {
+    "revision": "8fa07a5d1fb850938b86",
+    "url": "/infolanka-react/static/js/main.aeacce02.chunk.js"
+  },
+  {
+    "revision": "46a53b4801b8b6164dcb",
+    "url": "/infolanka-react/static/js/runtime~main.c5c069ca.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/modern-infolanka/precache-manifest.2b577cd7cbb3ece52a2ab0369fd8d0dd.js"
+  "/infolanka-react/precache-manifest.273ada15aaa6b4ca7fc260c8fafe3777.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/modern-infolanka/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/infolanka-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
